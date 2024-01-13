ba83c76671428d5ce94282473bed9768574bb8a5
Lucia-Cai <cai.lucia04@gmail.com>
2024-01-12 23:41:21 -0500

    added stuff

--- api/gmail/gmail.py
+++ api/gmail/gmail.py
@@ -39,6 +39,9 @@ def get_google_api_connection():
 
 
 def search_messages(service, query):
+    """
+    returns the IDs of the mails that match the query
+    """
     result = service.users().messages().list(userId='me',q=query).execute()
     messages = [ ]
     if 'messages' in result:
@@ -101,6 +104,61 @@ def parse_parts(service, parts, folder_name, message):
                                     f.write(urlsafe_b64decode(data))
 
 
+def read_message(service, message):
+    """
+    This function takes Gmail API `service` and the given `message_id` and does the following:
+        - Downloads the content of the email
+        - Prints email basic information (To, From, Subject & Date) and plain/text parts
+        - Creates a folder for each email based on the subject
+        - Downloads text/html content (if available) and saves it under the folder created as index.html
+        - Downloads any file that is attached to the email and saves it in the folder created
+    """
+    msg = service.users().messages().get(userId='me', id=message['id'], format='full').execute()
+    # parts can be the message body, or attachments
+    payload = msg['payload']
+    headers = payload.get("headers")
+    parts = payload.get("parts")
+    folder_name = "email"
+    has_subject = False
+    if headers:
+        # this section prints email basic info & creates a folder for the email
+        for header in headers:
+            name = header.get("name")
+            value = header.get("value")
+            if name.lower() == 'from':
+                # we print the From address
+                print("From:", value)
+            if name.lower() == "to":
+                # we print the To address
+                print("To:", value)
+            if name.lower() == "subject":
+                # make our boolean True, the email has "subject"
+                has_subject = True
+                # make a directory with the name of the subject
+                folder_name = clean(value)
+                # we will also handle emails with the same subject name
+                folder_counter = 0
+                while os.path.isdir(folder_name):
+                    folder_counter += 1
+                    # we have the same folder name, add a number next to it
+                    if folder_name[-1].isdigit() and folder_name[-2] == "_":
+                        folder_name = f"{folder_name[:-2]}_{folder_counter}"
+                    elif folder_name[-2:].isdigit() and folder_name[-3] == "_":
+                        folder_name = f"{folder_name[:-3]}_{folder_counter}"
+                    else:
+                        folder_name = f"{folder_name}_{folder_counter}"
+                os.mkdir(folder_name)
+                print("Subject:", value)
+            if name.lower() == "date":
+                # we print the date when the message was sent
+                print("Date:", value)
+    if not has_subject:
+        # if the email does not have a subject, then make a folder with "email" name
+        # since folders are created based on subjects
+        if not os.path.isdir(folder_name):
+            os.mkdir(folder_name)
+    parse_parts(service, parts, folder_name, message)
+    print("="*50)
 
 
 
