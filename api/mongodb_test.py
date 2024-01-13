3a96262e6183cfa2f9ea66be931e04dbd8c06cd8
Justin Bax <justin.bax@icloud.com>
2024-01-12 22:03:27 -0500

    added database interface

--- api/mongodb_test.py
+++ api/mongodb_test.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
-def get_database():
 
+def get_database():
     MONGO_URI = "mongodb+srv://brebeufhx:brebeufhx@brebeufhx.pdpnnja.mongodb.net/"
     client = MongoClient(MONGO_URI)
     try:
@@ -12,10 +12,16 @@ def get_database():
     return client["brebeufhx"]
 
 
-dbname = get_database()
-collection_name = dbname["tracks"]
+def get_mails(collection, filters):
+    return collection.find(filters)
 
-# Insert into database
+
+def push_mail(collection, mail):
+    collection.insert_one(mail)
+
+
+dbname = get_database()
+collection = dbname["tracks"]
 
 mail_1 = {
     "name": "John Smith",
@@ -24,10 +30,16 @@ mail_1 = {
     "status": "Not read",
     "desc": "Not read"
 }
-collection_name.insert_one(mail_1)
+collection.insert_one(mail_1)
 
 
 # Query and search the database
-mails = collection_name.find()
+mails = get_mails(collection, {})
+for mail in mails:
+    print(f"From {mail['name'] }: {mail['status']} ({mail['desc']})")
+
+# Only search the database for mails sent to def@gmail.com
+print("Sent to def@gmail.com:")
+mails = get_mails(collection, {"sent_to": "def@gmail.com"})
 for mail in mails:
     print(f"From {mail['name'] }: {mail['status']} ({mail['desc']})")
